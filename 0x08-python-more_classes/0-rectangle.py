5ba2481643737649ad12d5c170ea58c390de90cb
Natnael Dejene <122891062+NatiiiiiD@users.noreply.github.com>
2024-02-08 08:40:04 +0300

    Delete 0x08-python-more_classes directory

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-This defines an empty class Rectangle
-"""
-
-
-class Rectangle:
-    """Empty representation of a rectangle"""
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,1 +0,0 @@
-ALX python more classes project
